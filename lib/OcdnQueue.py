40ee124b4dc29622e9e203c0136c4eb7ddaca431
firefoxbug <wanghuafire@gmail.com>
2014-05-31 23:22:33 +0800

    opencdn pulic queue

---
+++ lib/OcdnQueue.py
@@ -0,0 +1,13 @@
+
+
+class Queue(object):
+	"""OpenCDN Queue control"""
+	def __init__(self, arg):
+		super(Queue, self).__init__()
+		self.arg = arg
+	
+	def get_item_from_queue(queue_name):
+		pass
+
+	def put_item_into_queue(queue_name):
+		pass
