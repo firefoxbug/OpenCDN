f3848f3c257f1f1ce44c64cac10cced1dbb15362
firefoxbug <wanghuafire@gmail.com>
2014-06-10 20:31:17 +0800

    增加redis队列，暂且未用

--- lib/OcdnQueue.py
+++ lib/OcdnQueue.py
@@ -38,7 +38,6 @@ class Producer(object):
 	1. producer need to connect to gearman
 	2. produce tasks and put them into queue"""
 	def __init__(self, queue_ip='127.0.0.1', queue_port=4730):
-#		self.conlog = init_logger(logfile='producer.log', stdout=True)
 		self.gearman_server_addr = "%s:%s"%(queue_ip,queue_port)
 		self.producer =None
 
@@ -105,6 +104,49 @@ class Consumer(gearman.GearmanWorker):
 	#		print str(e)
 			self.conlog.error('TaskModule: %s: Put task into queue. Data:%s'%(queue_name, data))
 			return False
+
+import redis
+
+class RedisQueue(object):
+	"""Simple Queue with Redis Backend
+
+	>>> redis_q = RedisQueue(host='127.0.0.1')
+	>>> redis_q.put('KEY', 'Value')
+	>>> redis_q.get('KEY')
+	"""
+	def __init__(self,**redis_kwargs):
+		"""The default connection parameters are: host='localhost', port=6379, db=0"""
+		self.__db= redis.Redis(**redis_kwargs)
+
+	def qsize(self,key):
+		"""Return the approximate size of the queue."""
+		return self.__db.llen(key)
+
+	def empty(self):
+		"""Return True if the queue is empty, False otherwise."""
+		return self.qsize() == 0  
+
+	def put(self,key,item):
+		"""Put item into the queue."""
+		self.__db.rpush(key, item)  
+
+	def get(self,key,block=True, timeout=None):
+		"""Remove and return an item from the queue.  
+
+		If optional args block is true and timeout is None (the default), block
+		if necessary until an item is available."""
+		if block:
+			item = self.__db.blpop(key, timeout=timeout)
+		else:
+			item = self.__db.lpop(key)  
+
+		if item:
+			item = item[1]
+		return item
+
+	def get_nowait(self):
+		"""Equivalent to get(False)."""
+		return self.get(False)
 
 if __name__ == '__main__':
 	Consumer.push_task(queue_ip='103.6.222.21', queue_port=4730, queue_name='OCDNQUEUE', data='hello')
